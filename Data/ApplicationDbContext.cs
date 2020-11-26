25643c9e463527fd2490eb78f3989b3df4aea502
thomasfregene <fregzythomas@gmail.com>
2020-11-26 18:26:07 +0100

    more on database configuration

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using DapperDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DapperDemo.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+
+        }
+        public DbSet<Company> Companies { get; set; } 
     }
 }
--- Models/Company.cs
+++ Models/Company.cs
@@ -7,5 +7,12 @@ namespace DapperDemo.Models
 {
     public class Company
     {
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+
     }
 }
