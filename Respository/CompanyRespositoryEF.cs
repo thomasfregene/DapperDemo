6ba5e35a99d5a25d6a3dd26cad14f3a385c37a98
thomasfregene <fregzythomas@gmail.com>
2020-11-26 23:22:52 +0100

    Replacing the dbcontext with compRepo -dependency injection

--- Respository/CompanyRespositoryEF.cs
+++ Respository/CompanyRespositoryEF.cs
@@ -1,11 +1,50 @@
-﻿using System;
+﻿using DapperDemo.Data;
+using DapperDemo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DapperDemo.Respository
 {
-    public class CompanyRespositoryEF
+    public class CompanyRespositoryEF : ICompanyRespository
     {
+        private readonly ApplicationDbContext _db;
+
+        public CompanyRespositoryEF(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public Company Add(Company company)
+        {
+            _db.Companies.Add(company);
+            _db.SaveChanges();
+            return company;
+        }
+
+        public Company Find(int id)
+        {
+            return _db.Companies.FirstOrDefault(u=>u.CompanyId == id);
+        }
+
+        public List<Company> GetAll()
+        {
+            return _db.Companies.ToList();
+        }
+
+        public void Remove(int id)
+        {
+            Company company = _db.Companies.FirstOrDefault(u => u.CompanyId == id);
+            _db.Companies.Remove(company);
+            _db.SaveChanges();
+            return;
+        }
+
+        public Company Update(Company company)
+        {
+            _db.Companies.Update(company);
+            _db.SaveChanges();
+            return company;
+        }
     }
 }
--- Respository/ICompanyRespository.cs
+++ Respository/ICompanyRespository.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using DapperDemo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DapperDemo.Respository
 {
-    interface ICompanyRespository
+    public interface ICompanyRespository
     {
+        Company Find(int id);
+        List<Company> GetAll();
+        Company Add(Company company);
+        Company Update(Company company);
+        void Remove(int id);
     }
 }
