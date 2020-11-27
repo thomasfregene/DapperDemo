a4c9380f3bfa63e4f1e5f0e1b86ba8e514d05853
thomasfregene <fregzythomas@gmail.com>
2020-11-27 09:48:58 +0100

    Implementing the GetAll And GetById(find) method -DapperRepo

--- Respository/CompanyRespository.cs
+++ Respository/CompanyRespository.cs
@@ -1,11 +1,49 @@
-﻿using System;
+﻿using Dapper;
+using DapperDemo.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DapperDemo.Respository
 {
-    public class CompanyRespository
+    public class CompanyRespository : ICompanyRespository
     {
+        //dapper
+        IDbConnection db;
+        public CompanyRespository(IConfiguration configuration)
+        {
+            this.db = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public Company Add(Company company)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Company Find(int id)
+        {
+            var sql = "SELECT * FROM Companies WHERE CompanyId = @CompanyId";
+            return db.Query<Company>(sql, new { @CompanyId = id }).Single();
+        }
+
+        public List<Company> GetAll()
+        {
+            var sql = "SELECT * FROM Companies";
+            return db.Query<Company>(sql).ToList();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Company Update(Company company)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
